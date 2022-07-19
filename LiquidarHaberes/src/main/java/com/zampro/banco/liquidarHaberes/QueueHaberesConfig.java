e4adeb6c5a79dc7b2d29d04a26c084948fd6d4ec
Juan Zampronio <juan.f.zampronio@accenture.com>
2022-07-19 16:49:06 -0300

    Microservicio Liquidar Haberes

--- LiquidarHaberes/src/main/java/com/zampro/banco/liquidarHaberes/QueueHaberesConfig.java
+++ LiquidarHaberes/src/main/java/com/zampro/banco/liquidarHaberes/QueueHaberesConfig.java
@@ -7,8 +7,10 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.command.ActiveMQQueue;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.core.JmsTemplate;
 
+@Configuration
 public class QueueHaberesConfig {
 	
 	@Value("${activemq.broker.url}")
